809ecd0546bc566836c53a894d3bdce6e5090503
EricTran6 <e.ktran06@gmail.com>
2025-10-22 18:37:39 -0700

    Time: 11 ms (71.98%), Space: 18.1 MB (33.01%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -4,10 +4,9 @@ class Solution:
             return False
         arrS = {}
         arrT = {}
-        for ch in s:
-            arrS[ch] = arrS.get(ch, 0) + 1
-        for ch in t:
-            arrT[ch] = arrT.get(ch, 0) + 1
+        for ch in range(len(s)):
+            arrS[s[ch]] = arrS.get(s[ch], 0) + 1
+            arrT[t[ch]] = arrT.get(t[ch], 0) + 1
         if arrS != arrT:
             return False
         return True
